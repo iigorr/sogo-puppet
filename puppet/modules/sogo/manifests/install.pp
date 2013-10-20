0b714e515a9642d10e7d312c5f112b7a8e2ed040
Igor Lankin <dev@lankin.net>
2013-10-20 02:16:27 +0200

    added "unless/onlyif"-rules so apt-stuff isn't run each time

--- puppet/modules/sogo/manifests/install.pp
+++ puppet/modules/sogo/manifests/install.pp
@@ -11,19 +11,21 @@ class sogo::install {
     path      => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     command   => 'apt-key adv --keyserver keys.gnupg.net --recv-key 0x810273C4',
     subscribe => File['/etc/apt/sources.list.d/sogo.list'],
+    unless    => '/usr/bin/apt-key finger| /bin/grep "8102 73C4"'
   }
 
   exec { 'apt-update-sogo':
     path      => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     command   => 'apt-get update',
     require => Exec['add-sogo-key'],
+    onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
   }
 
   package { 'sope4.9-gdl1-mysql':
     ensure => installed,
     require => Exec['apt-update-sogo'],
   }
-  
+
   package { 'sogo':
     ensure  => installed,
     require => Exec['apt-update-sogo'],
--- puppet/modules/system-base/manifests/init.pp
+++ puppet/modules/system-base/manifests/init.pp
@@ -3,6 +3,7 @@ class system-base {
   exec { "apt-get-init":
     path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
     command => "apt-get update",
+    onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
   }
 
 
